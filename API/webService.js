13081a7c75606515d2c6903d2004418f41eb3e9e
Shayzaad Kallan <shayzaadkallan786@gmail.com>
2025-08-04 12:44:41 +0200

    Update webService.js

--- API/webService.js
+++ API/webService.js
@@ -43,6 +43,7 @@ app.use(cors({
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://127.0.0.1:3000',
+    'precious-consideration-production.up.railway.app',
     'http://localhost' //Added for Yatin's frontend
   ],
   credentials: true,
