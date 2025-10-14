c13144ef7bc2b2e21c400ea287e24917aa0b6085
YatinFakir <yatinfakir@gmail.com>
2025-10-14 13:39:04 +0200

    Update vue-event-management.js

--- API/Front_End_Web/js/vue-event-management.js
+++ API/Front_End_Web/js/vue-event-management.js
@@ -920,7 +920,7 @@ document.addEventListener('DOMContentLoaded', () => {
   try {
     const date = new Date(dateString);
 
-    // Check if the date is valid
+    //Check if the date is valid
     if (isNaN(date.getTime())) {
       return "Invalid Date";
     }
